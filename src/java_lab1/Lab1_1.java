407282826eaa0173d789dae14bbf1813652921c2
Lyf0202 <674745781@qq.com>
2015-12-23 13:38:24 +0800

    first Code Manage

---
+++ src/java_lab1/Lab1_1.java
@@ -0,0 +1,58 @@
+package java_lab1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+ * 梁焱烽 2013213463
+ * Java programming Fall 2015-2016
+ * Lab 1
+ */
+public class Lab1_1 {
+
+	public static Map<String, String> getTime(long seconds) {
+
+		seconds = seconds < 0? -1*seconds : seconds;
+		Map<String, String> map = new HashMap<String, String>();
+		int time = 0;
+		String type = "";
+		if (seconds >= 86400) {
+			time = (int) (seconds / 86400);
+			type = "days";
+		} else if (seconds >= 3600) {
+			time = (int) (seconds / 3600);
+			type = "hours";
+		} else {
+			time = (int) (seconds / 60);
+			type = "minutes";
+		}
+		map.put("type", type);
+		map.put("time", String.valueOf(time));
+		return map;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i < 3; i++) {
+			Scanner in = new Scanner(System.in);
+			System.out.print("Enter the number of seconds: ");
+			long seconts = 0;
+			try {
+				seconts = in.nextLong();
+				Map<String, String> map = Lab1_1.getTime(seconts);
+				System.out.println("The number of " + map.get("type")
+						+ " in that many seconds is: " + map.get("time"));
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("无效输入!");
+				i--;
+			}
+		}
+	}
+
+}
---
+++ src/java_lab1/Lab1_2.java
@@ -0,0 +1,43 @@
+package java_lab1;
+
+import java.util.Scanner;
+
+public class Lab1_2 {
+
+	
+	public static void showCountNum(double startingNum,double increase,int days){	
+		for(int i = 1;i<=days;i++){
+			System.out.print("Day "+i+": ");
+			startingNum = startingNum * (1 + increase);
+			System.out.println(startingNum);
+		}
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double startingNum = 0;
+		double increase = 0;
+		int days = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the starting number of organism:");
+		startingNum = in.nextDouble();
+		System.out.print("Enter the daily increase(percentage):");
+		increase = in.nextDouble();
+		System.out.print("Enter the number of days:");
+		days = in.nextInt();
+		while(startingNum < 2 || increase < 0 || days < 1){
+			System.out.println("The input is invalid!");
+			System.out.print("Enter the starting number of organism:");
+			startingNum = in.nextDouble();
+			System.out.print("Enter the daily increase(percentage):");
+			increase = in.nextDouble();
+			System.out.print("Enter the number of days:");
+			days = in.nextInt();
+		}
+		System.out.println();
+		showCountNum(startingNum, increase, days);
+	}
+
+}
